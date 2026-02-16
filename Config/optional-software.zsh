0bfa61c1c0f866001ab567b2ef854d080866de70
Seth Junot <xsetech@gmail.com>
2026-02-16 12:33:12 -0800

    [optional-software.zsh] ldflags, cxxflags, refactor
    Changes:
    * New _config_prepend_static_libs: when present, static libraries from
      that directory are placed at the very beginning of LDFLAGS, before all
      -L entries from any directory
    * New _config_ldflags: reads custom LDFLAGS from file content and adds
      them to LDFLAGS for that directory
    * Added ACTIVE_OPTIONAL_CXXFLAGS that mirrors ACTIVE_OPTIONAL_CPPFLAGS
      for CMake compatibility

--- Config/optional-software.zsh
+++ Config/optional-software.zsh
@@ -3,23 +3,56 @@
 # also referred to as a "prefix" (as in `./configure --prefix=...`).
 #
 # Special configuration files (placed in lib/ or lib64/ directories):
-#   _config_no_l_flag         - If present, suppress the "-L<dir>" flag for this directory
-#   _config_append_static_libs - If present, append all static libraries (.a files) from
-#                                this directory directly to LDFLAGS with absolute paths
+#   _config_no_l_flag           - If present, suppress the "-L<dir>" flag for this directory
+#   _config_append_static_libs  - If present, append all static libraries (.a files) from
+#                                 this directory directly to LDFLAGS with absolute paths
 #   _config_prepend_static_libs - If present, prepend all static libraries (.a files) from
 #                                 this directory before ALL -L entries in LDFLAGS
+#   _config_ldflags             - If present, content is read and added to LDFLAGS for this directory
 function configure_active_optional_software() {
 
     export ACTIVE_OPTIONAL_PATH=""
     export ACTIVE_OPTIONAL_LD_LIBRARY_PATH=""
     export ACTIVE_OPTIONAL_LDFLAGS=""
     export ACTIVE_OPTIONAL_CPPFLAGS=""
+    export ACTIVE_OPTIONAL_CXXFLAGS=""
     export ACTIVE_OPTIONAL_MANPATH=""
     export ACTIVE_OPTIONAL_PKGCONFIG=""
 
     # Temporary variables to collect static libs that should be prepended
     local PREPEND_STATIC_LIBS=""
     local REGULAR_LDFLAGS=""
+
+    # Helper function to process lib or lib64 directories
+    function process_lib_dir() {
+        local lib_dir=$1
+
+        export ACTIVE_OPTIONAL_LD_LIBRARY_PATH="${ACTIVE_OPTIONAL_LD_LIBRARY_PATH}:${lib_dir}"
+
+        # Handle static libraries based on configuration
+        if [[ -e ${lib_dir}/_config_prepend_static_libs ]]; then
+            # Collect static libs to prepend before ALL -L entries
+            for static_lib in ${lib_dir}/*.a(N-.); do
+                PREPEND_STATIC_LIBS="${PREPEND_STATIC_LIBS} ${static_lib}"
+            done
+        elif [[ -e ${lib_dir}/_config_append_static_libs ]]; then
+            # Append static libraries before -L flag for this directory
+            for static_lib in ${lib_dir}/*.a(N-.); do
+                REGULAR_LDFLAGS="${REGULAR_LDFLAGS} ${static_lib}"
+            done
+        fi
+
+        # Add custom LDFLAGS from _config_ldflags file
+        if [[ -e ${lib_dir}/_config_ldflags ]]; then
+            local custom_ldflags=$(cat ${lib_dir}/_config_ldflags)
+            REGULAR_LDFLAGS="${REGULAR_LDFLAGS} ${custom_ldflags}"
+        fi
+
+        # Only add -L flag if _config_no_l_flag is not present
+        if [[ ! -e ${lib_dir}/_config_no_l_flag ]]; then
+            REGULAR_LDFLAGS="${REGULAR_LDFLAGS} -L${lib_dir}"
+        fi
+    }
 
     if [[ -d $LOCAL/active ]]; then
 
@@ -44,51 +77,16 @@ function configure_active_optional_software() {
                     fi
 
                     if [[ -e ${active_entry_abs_path}/lib ]]; then
-                        export ACTIVE_OPTIONAL_LD_LIBRARY_PATH="${ACTIVE_OPTIONAL_LD_LIBRARY_PATH}:${active_entry_abs_path}/lib"
-
-                        # Handle static libraries based on configuration
-                        if [[ -e ${active_entry_abs_path}/lib/_config_prepend_static_libs ]]; then
-                            # Collect static libs to prepend before ALL -L entries
-                            for static_lib in ${active_entry_abs_path}/lib/*.a(N-.); do
-                                PREPEND_STATIC_LIBS="${PREPEND_STATIC_LIBS} ${static_lib}"
-                            done
-                        elif [[ -e ${active_entry_abs_path}/lib/_config_append_static_libs ]]; then
-                            # Append static libraries before -L flag for this directory
-                            for static_lib in ${active_entry_abs_path}/lib/*.a(N-.); do
-                                REGULAR_LDFLAGS="${REGULAR_LDFLAGS} ${static_lib}"
-                            done
-                        fi
-
-                        # Only add -L flag if _config_no_l_flag is not present
-                        if [[ ! -e ${active_entry_abs_path}/lib/_config_no_l_flag ]]; then
-                            REGULAR_LDFLAGS="${REGULAR_LDFLAGS} -L${active_entry_abs_path}/lib"
-                        fi
+                        process_lib_dir ${active_entry_abs_path}/lib
                     fi
 
                     if [[ -e ${active_entry_abs_path}/lib64 ]]; then
-                        export ACTIVE_OPTIONAL_LD_LIBRARY_PATH="${ACTIVE_OPTIONAL_LD_LIBRARY_PATH}:${active_entry_abs_path}/lib64"
-
-                        # Handle static libraries based on configuration
-                        if [[ -e ${active_entry_abs_path}/lib64/_config_prepend_static_libs ]]; then
-                            # Collect static libs to prepend before ALL -L entries
-                            for static_lib in ${active_entry_abs_path}/lib64/*.a(N-.); do
-                                PREPEND_STATIC_LIBS="${PREPEND_STATIC_LIBS} ${static_lib}"
-                            done
-                        elif [[ -e ${active_entry_abs_path}/lib64/_config_append_static_libs ]]; then
-                            # Append static libraries before -L flag for this directory
-                            for static_lib in ${active_entry_abs_path}/lib64/*.a(N-.); do
-                                REGULAR_LDFLAGS="${REGULAR_LDFLAGS} ${static_lib}"
-                            done
-                        fi
-
-                        # Only add -L flag if _config_no_l_flag is not present
-                        if [[ ! -e ${active_entry_abs_path}/lib64/_config_no_l_flag ]]; then
-                            REGULAR_LDFLAGS="${REGULAR_LDFLAGS} -L${active_entry_abs_path}/lib64"
-                        fi
+                        process_lib_dir ${active_entry_abs_path}/lib64
                     fi
 
                     if [[ -e ${active_entry_abs_path}/include ]]; then
                         export ACTIVE_OPTIONAL_CPPFLAGS="${ACTIVE_OPTIONAL_CPPFLAGS} -I${active_entry_abs_path}/include"
+                        export ACTIVE_OPTIONAL_CXXFLAGS="${ACTIVE_OPTIONAL_CXXFLAGS} -I${active_entry_abs_path}/include"
                     fi
 
                     if [[ -e ${active_entry_abs_path}/share/man ]]; then
@@ -118,4 +116,3 @@ function configure_active_optional_software() {
         echo "" >&2
     fi
 }
-
